1f96c7b05f7d14e372848922be70bd4ee2c1eadd
Yasmin Maoz-Nygate <yasmin.nygate@gmail.com>
2019-07-11 15:16:59 +0300

    starting

---
+++ src/components/Layout/Layout.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="layout">
+    <test :data="menu"/>
+  </div>
+</template>
+
+<script>
+  import test from '../test';
+
+  const menu = [
+    {
+      name: 'Feed',
+      path: '/',
+    }, {
+      name: 'New Quote',
+      path: '/new',
+    }, {
+      name: 'Profile',
+      path: '/profile/me',
+    }, {
+      name: 'Settings',
+      path: '/settings',
+    }, {
+      name: 'Search',
+      path: '/search',
+    }, {
+      name: 'Notifications',
+      path: '/notifications',
+    }, {
+      name: 'Messages',
+      path: '/messages',
+    },
+  ];
+
+  export default {
+    name: 'Layout',
+    data() {
+      return {
+        menu,
+      };
+    },
+    components: { test },
+  };
+</script>
+
+<style>
+
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,32 @@
+<template>
+  <carousel :navigationEnabled="true" :centerMode="true" :paginationEnabled="false" :perPage="5" :spacePadding="10">
+      <slide v-for="value in data">
+        <div class="item-style">
+          <a :href="value.path">
+            <h2>{{ value.name }}</h2>
+          </a>
+        </div>
+      </slide>
+  </carousel>
+</template>
+
+<script>
+  import { Carousel, Slide } from 'vue-carousel';
+
+  export default {
+    name: 'test',
+    components: {
+      carousel: Carousel,
+      slide: Slide,
+    },
+    props: ['data'],
+  };
+</script>
+
+<style>
+  .VueCarousel {
+    width: 80%;
+    margin: 0 auto;
+  }
+
+</style>
